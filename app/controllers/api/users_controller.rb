072129754e077d499369ae94424b93efa03d814c
Younis <younis.hamalawy@gmail.com>
2017-07-18 16:37:25 -0700

    sessions controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,19 +1,19 @@
 class Api::UsersController < ApplicationController
 
   def create
-		@user = User.new(user_params)
+  	@user = User.new(user_params)
 
-		if @user.save
-			sign_in(@user)
-			render "/"
-		else
-			render json: @user.errors.full_messages, status: 422
-		end
-	end
+  	if @user.save
+  		sign_in(@user)
+  		render "api/users/show"
+  	else
+  		render json: @user.errors.full_messages, status: 422
+  	end
+  end
 
-	private
+  private
 
-	def user_params
-		params.require(:user).permit(:email, :password)
-	end
+  def user_params
+  	params.require(:user).permit(:email, :password)
+  end
 end
