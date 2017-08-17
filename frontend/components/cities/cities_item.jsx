d3f5ea55e5f68d621381903c24542a9536decaa5
Younis <younis.hamalawy@gmail.com>
2017-08-17 11:38:30 -0700

    move font loading from main css to application.html.erb

--- frontend/components/cities/cities_item.jsx
+++ frontend/components/cities/cities_item.jsx
@@ -4,31 +4,30 @@ import { Link } from 'react-router-dom';
 
 class CitiesItem extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.handleClick = this.handleClick.bind(this);
     this.setCityButton = this.setCityButton.bind(this);
-    }
+  }
 
-    handleClick(e) {
-      e.preventDefault();
-      let newUser = Object.assign({}, this.props.currentUser);
-      newUser["city_id"] = this.props.city.id;
-      this.props.setCity(this.props.currentUser.id, newUser)
-    }
-
-    setCityButton() {
-      if (this.props.currentUser){
-        if (this.props.currentUser.city_id === this.props.city.id) {
-          return  (<p className="p1">Home City</p>)
-          }else{
-            return (
-              <a className="button-link" href="#" onClick={this.handleClick}>set as home city</a>)  }
-
+  setCityButton() {
+    if (this.props.currentUser) {
+      if (this.props.currentUser.city_id === this.props.city.id) {
+        return (<p className="p1">Home City</p>);
       }
-    }
-
-    render () {
       return (
+        <a className="button-link" href="#" onClick={this.handleClick}>set as home city</a>
+      );
+    }
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    const newUser = Object.assign({}, this.props.currentUser);
+    newUser.city_id = this.props.city.id;
+    this.props.setCity(this.props.currentUser.id, newUser);
+  }
+  render() {
+    return (
       <div className="city-container">
         <div key={this.props.city.id} className={this.props.city.city_abrv}>
           <div className="transbox">
@@ -38,7 +37,7 @@ class CitiesItem extends React.Component {
         {this.setCityButton()}
 
       </div>
-      )
+    );
   }
 }
 
