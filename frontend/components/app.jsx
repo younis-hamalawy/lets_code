ee6eabc7a6843ecded16e808b4acc9458333ee74
Younis <younis.hamalawy@gmail.com>
2017-08-23 21:43:03 -0700

    fix indentation in app.jsx

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,24 +3,24 @@ import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
 import SessionFormContainer from './session_form/session_form_container';
 import FooterContainer from './footer/footer_container';
-import CitiesContainer from './cities/cities_container'
-import CityContainer from './city/city_container'
-import SplashContainer from './splash/splash_container'
-import HostingFormContainer from './hosting/hosting_form_container'
-import DashboardContainer from './dashboard/dashboard_container'
+import CitiesContainer from './cities/cities_container';
+import CityContainer from './city/city_container';
+import SplashContainer from './splash/splash_container';
+import HostingFormContainer from './hosting/hosting_form_container';
+import DashboardContainer from './dashboard/dashboard_container';
 
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
   <div className="main-app">
-      <navbar className="navbar">
-        <div className="logo">
-          <img src="https://res.cloudinary.com/flying-monkey/image/upload/c_crop,h_377,w_987/v1501200502/L_C_tshqyx.png" alt="logo" height="50" width="100" className="logo-img"></img>
-          <Link to="/"><h1>Let's Code!</h1></Link>
-        </div>
-        <GreetingContainer className="button-container"/>
-      </navbar>
-      <div className="main-page">
+    <navbar className="navbar">
+      <div className="logo">
+        <img src="https://res.cloudinary.com/flying-monkey/image/upload/c_crop,h_377,w_987/v1501200502/L_C_tshqyx.png" alt="logo" height="50" width="100" className="logo-img" />
+        <Link to="/"><h1>Let's Code!</h1></Link>
+      </div>
+      <GreetingContainer className="button-container" />
+    </navbar>
+    <div className="main-page">
       <Switch>
         <AuthRoute exact path="/signin" component={SessionFormContainer} />
         <AuthRoute exact path="/signup" component={SessionFormContainer} />
