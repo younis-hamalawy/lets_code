b91861ac0176bf98441957edf86a4d19fe3d337b
Younis <younis.hamalawy@gmail.com>
2017-08-23 19:32:18 -0700

    fix webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,46 @@
-var path = require("path");
-var webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
-var plugins = []; // if using any plugins for both dev and production
-var devPlugins = []; // if using any plugins for development
+let plugins = [];
+const devPlugins = [];
 
-var prodPlugins = [
+const prodPlugins = [
   new webpack.DefinePlugin({
     'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
+      NODE_ENV: JSON.stringify('production'),
+    },
   }),
   new webpack.optimize.UglifyJsPlugin({
     compress: {
-      warnings: true
-    }
-  })
+      warnings: true,
+    },
+  }),
 ];
 
-plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-)
+plugins = plugins.concat(process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins);
 
-// include plugins config
 module.exports = {
   context: __dirname,
-  entry: "./frontend/lets_code.jsx",
+  entry: './frontend/lets_code.jsx',
   output: {
-    path: path.resolve(__dirname, "app", "assets", "javascripts"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js',
   },
-  plugins: plugins,
+  plugins,
   module: {
     loaders: [
       {
-
-        test: /\.jsx?$/,
+        test: [/\.jsx?$/, /\.js?$/],
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
-        }
-      }
-    ]
+          presets: ['es2015', 'react'],
+        },
+      },
+    ],
   },
   devtool: 'source-map',
   resolve: {
-    extensions: [".js", ".jsx", "*"]
-  }
+    extensions: ['.js', '.jsx', '*'],
+  },
 };
